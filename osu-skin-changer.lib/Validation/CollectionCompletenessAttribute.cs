ec5b7d583e9989419b2c193b797880c3f3e706d8
sad_degenerate <mister.franz02@gmail.com>
2022-10-15 15:00:31 +0700

    Created base validation.

---
+++ osu-skin-changer.lib/Validation/CollectionCompletenessAttribute.cs
@@ -0,0 +1,75 @@
+﻿using osu_skin_changer.lib.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace osu_skin_changer.lib.Validation
+{
+    public class CollectionCompletenessAttribute : ValidationAttribute
+    {
+        private readonly string[] _expectedContent;
+        private readonly Type _type;
+        public CollectionCompletenessAttribute(string[] expectedContent, Type type)
+        {
+            if (expectedContent == null || expectedContent.Length == 0)
+            {
+                throw new ArgumentNullException(nameof(expectedContent), "Expected content is empty.");
+            }
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type), "Type is null.");
+            }
+
+            _expectedContent = expectedContent;
+            _type = type;
+        }
+
+        public override bool IsValid(object? value)
+        {
+            if (_type == typeof(Sound))
+            {
+                if (CompareContents(value as IEnumerable<Sound>) == false)
+                {
+                    ErrorMessage = "Collection content is not including expected content.";
+                    return false;
+                }
+            }
+            else if (_type == typeof(Image))
+            {
+                if (CompareContents(value as IEnumerable<Image>) == false)
+                {
+                    ErrorMessage = "Collection content is not including expected content.";
+                    return false;
+                }
+            }
+            else
+            {
+                ErrorMessage = "Collection type is not allowed.";
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool CompareContents<T>(IEnumerable<T> collection) where T : BaseObject
+        {
+            foreach (var expectedContent in _expectedContent)
+            {
+                var isContentExists = false;
+                foreach (var content in collection)
+                {
+                    if (content.Name == expectedContent)
+                    {
+                        isContentExists = true;
+                        break;
+                    }
+                }
+                
+                if (isContentExists == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ osu-skin-changer.lib/Validation/MediaFileAttribute.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace osu_skin_changer.lib.Validation
+{
+    public class MediaFileAttribute : ValidationAttribute
+    {
+        private readonly string[] _allowedExtensions = { ".png" };
+
+        public MediaFileAttribute(string[] allowedExtensions)
+        {
+            if (allowedExtensions == null || allowedExtensions.Length == 0)
+            {
+                throw new ArgumentNullException(nameof(allowedExtensions), "Allowed extensions is null or empty.");
+            }
+
+            _allowedExtensions = allowedExtensions;
+        }
+
+        public override bool IsValid(object? value)
+        {
+            if (value != null)
+            {
+                var path = value.ToString();
+                if (File.Exists(path))
+                {
+                    var isValidExtension = false;
+                    foreach (var extension in _allowedExtensions)
+                    {
+                        if (Path.GetExtension(path) == extension)
+                        {
+                            isValidExtension = true;
+                            break;
+                        }
+                    }
+                    
+                    if (isValidExtension == true)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        ErrorMessage = $"File extension is not valid.\nValid extensions: {_allowedExtensions}.";
+                    }
+                }
+                else
+                {
+                    ErrorMessage = "File is not exist.";
+                }
+            }
+            else
+            {
+                ErrorMessage = "File is not exist.";
+            }
+
+            return false;
+        }
+    }
+}
