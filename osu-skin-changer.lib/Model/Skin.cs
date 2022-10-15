b07acec622bc1f27c9aa1b3879120b86f53cf8fc
sad_degenerate <mister.franz02@gmail.com>
2022-10-15 18:05:53 +0700

    Updated skin model.

--- osu-skin-changer.lib/Model/Skin.cs
+++ osu-skin-changer.lib/Model/Skin.cs
@@ -1,5 +1,4 @@
-﻿using osu_skin_changer.lib.Validation;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace osu_skin_changer.lib.Model
 {
@@ -22,6 +21,27 @@ namespace osu_skin_changer.lib.Model
         public FileCollection<Sound> NormalHitsounds { get; set; }
         public FileCollection<Sound> DrumHitsounds { get; set; }
         public FileCollection<Sound> NightcoreHitsounds { get; set; }
+        public FileCollection<Sound> SpinnerSounds { get; set; }
+
+        public FileCollection<Image> Cursor { get; set; }
+        public FileCollection<Image> FollowPoint { get; set; }
+        public FileCollection<Image> Interface { get; set; }
+        public FileCollection<Image> Miss { get; set; }
+        public FileCollection<Image> Hit50 { get; set; }
+        public FileCollection<Image> Hit100 { get; set; }
+        public FileCollection<Image> Hit300 { get; set; }
+        public FileCollection<Image> HitCircle { get; set; }
+        public FileCollection<Image> RankingPanelHits { get; set; }
+        public FileCollection<Image> RankingPanelRanks { get; set; }
+        public FileCollection<Image> Ranks { get; set; }
+        public FileCollection<Image> InGameInterface { get; set; }
+        public FileCollection<Image> RankingPanel { get; set; }
+        public FileCollection<Image> Score { get; set; }
+        public FileCollection<Image> Default { get; set; }
+        public FileCollection<Image> Combo { get; set; }
+        public FileCollection<Image> ModIcons { get; set; }
+        public FileCollection<Image> Spinner { get; set; }
+        public FileCollection<Image> WelcomeText { get; set; }
 
         public Settings Settings { get; set; }
 
